2111be412231d25f71c53dffb953352c782a39bd
UGEL-Ucayali <oscar.ares@ugelcp.gob.pe>
2025-11-16 22:33:05 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Este archivo se genera automáticamente en el deploy
+// NO editar manualmente
+const FIREBASE_CONFIG = window.FIREBASE_CONFIG || {
+  apiKey: "PLACEHOLDER",
+  authDomain: "PLACEHOLDER",
+  projectId: "PLACEHOLDER",
+  storageBucket: "PLACEHOLDER",
+  messagingSenderId: "PLACEHOLDER",
+  appId: "PLACEHOLDER"
+};
+
+export default FIREBASE_CONFIG;
