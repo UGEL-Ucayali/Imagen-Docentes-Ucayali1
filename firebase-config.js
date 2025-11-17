110b79e041655b6ec7ccd916eda134ed0730ce65
UGEL-Ucayali <oscar.ares@ugelcp.gob.pe>
2025-11-16 23:06:10 -0500

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyD_l_MoWMWAlvZ7Ii5X6xQ7u5B1QSQP7JE", 
-  authDomain: "controldocente-ucayali.firebaseapp.com", 
-  projectId: "controldocente-ucayali", 
-  storageBucket: "controldocente-ucayali.firebasestorage.app", 
-  messagingSenderId: "473371376682", 
-  appId: "1:473371376682:web:f0766027414b27aed05753" 
-};
